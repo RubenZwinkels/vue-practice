5c3d32d2726b800f40e332462ab9cb98bf1ea8a4
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-03-14 15:53:27 +0100

    eerste funcionaliteit toegevoegd

---
+++ first_page/app.js
@@ -0,0 +1,10 @@
+const buttonEl = document.querySelector("button");
+const inputEl = document.querySelector("input");
+const listEl = document.querySelector("ul");
+
+buttonEl.addEventListener("click", addGoalToList);
+
+function addGoalToList(){
+	const inputValue = inputEl.value;
+	const goalListItem = inputValue.createElement();
+}
