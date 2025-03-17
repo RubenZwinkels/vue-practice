be652ea8aa614f501f15d6cc6978e169affd51f7
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-03-17 16:12:28 +0100

    [first-page] oude js verwijderd
    dit omdat het niet nuttig meer is
    Nu is het schoner

--- first_page/app.js
+++ first_page/app.js
@@ -1,17 +1,3 @@
-//const buttonEl = document.querySelector("button");
-//const inputEl = document.querySelector("input");
-//const listEl = document.querySelector("ul");
-//
-//buttonEl.addEventListener("click", addGoalToList);
-//
-//function addGoalToList() {
-//const inputValue = inputEl.value;
-//const listItemEl = document.createElement("li");
-//listItemEl.textContent = inputValue;
-//listEl.appendChild(listItemEl);
-//inputEl.value = "";
-//}
-
 Vue.createApp({
 	data() {
 		return {
