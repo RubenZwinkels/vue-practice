84dd399791be16a43b3195ae18739854a0fcaa63
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-03-17 16:09:48 +0100

    vue geimplementeerd ipv vanilla js

--- first_page/app.js
+++ first_page/app.js
@@ -1,13 +1,27 @@
-const buttonEl = document.querySelector("button");
-const inputEl = document.querySelector("input");
-const listEl = document.querySelector("ul");
+//const buttonEl = document.querySelector("button");
+//const inputEl = document.querySelector("input");
+//const listEl = document.querySelector("ul");
+//
+//buttonEl.addEventListener("click", addGoalToList);
+//
+//function addGoalToList() {
+//const inputValue = inputEl.value;
+//const listItemEl = document.createElement("li");
+//listItemEl.textContent = inputValue;
+//listEl.appendChild(listItemEl);
+//inputEl.value = "";
+//}
 
-buttonEl.addEventListener("click", addGoalToList);
-
-function addGoalToList() {
-	const inputValue = inputEl.value;
-	const listItemEl = document.createElement("li");
-	listItemEl.textContent = inputValue;
-	listEl.appendChild(listItemEl);
-	inputEl.value = "";
-}
+Vue.createApp({
+	data() {
+		return {
+			goals: [],
+			enteredValue: "",
+		};
+	},
+	methods: {
+		addGoal() {
+			this.goals.push(this.enteredValue);
+		}
+	}
+}).mount("#app");
