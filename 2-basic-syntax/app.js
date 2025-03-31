c81eed5b27062c922ad76530f93eebf1e02a03ab
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-03-31 13:57:12 +0200

    [2] simpele method toegevoegd

--- 2-basic-syntax/app.js
+++ 2-basic-syntax/app.js
@@ -5,5 +5,17 @@ const app = Vue.createApp({
       vueLink: "https://google.com",
     };
   },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber > 0.5) {
+        return "taakje 1";
+      } else {
+        return "taakje 2!";
+      }
+    },
+  },
 });
 app.mount("#user-goal");
+
+//console.log(outputgoal());
