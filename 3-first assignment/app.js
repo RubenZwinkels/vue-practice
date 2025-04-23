b2a985ee5b6ef08d554dd7ef6cece5632905f6ae
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-04-23 16:07:01 +0200

    [3] leeftijd toegevoeg
    met style afhankelijk van je leeftijd.

--- 3-first assignment/app.js
+++ 3-first assignment/app.js
@@ -2,8 +2,20 @@ Vue.createApp({
 	data() {
 		return {
 			name: "Ruubje",
+			age: 30,
 		};
 	},
 	methods: {
-	}
+		return_age() {
+			if (this.age < 30) {
+				return `<h3 style="color: pink">${this.age}</h3>`;
+			} else if (this.age >= 30 < 60) {
+				return `<h2 style="color: red">${this.age}</h2>`;
+			} else if (this.age >= 60) {
+				return `<h1 style="color: black">${this.age}</h1>`;
+			} else {
+				return "ongeldige leeftijd";
+			}
+		},
+	},
 }).mount("#assignment");
