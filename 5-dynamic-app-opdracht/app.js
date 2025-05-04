16d5ab547bd253135fa33747a862a45173988327
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-05-04 15:25:05 +0200

    [5] show text

--- 5-dynamic-app-opdracht/app.js
+++ 5-dynamic-app-opdracht/app.js
@@ -1,11 +1,15 @@
 Vue.createApp({
 	data() {
 		return {
+			text: "",
 		};
 	},
 	methods: {
-		show_alert(){
+		show_alert() {
 			alert("Wat goed dat je geklikt hebt");
-		}
-	}
+		},
+		register_text(event) {
+			this.text = event.target.value;
+		},
+	},
 }).mount("#assignment");
